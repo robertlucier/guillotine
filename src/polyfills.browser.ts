e5ae1e358bd95d50a55e26fdedbcc68383e97687
Rob Lucier <rob@treebbb.com>
2016-09-22 09:54:19 -0700

    add intl polyfill for safari compatibility with pipes

--- src/polyfills.browser.ts
+++ src/polyfills.browser.ts
@@ -27,6 +27,10 @@ import 'core-js/es6/reflect';
 // see issue https://github.com/AngularClass/angular2-webpack-starter/issues/709
 // import 'core-js/es6/promise';
 
+// workaround for missing Safari Intl 
+import 'intl';
+import 'intl/locale-data/jsonp/en-US'
+
 import 'core-js/es7/reflect';
 import 'zone.js/dist/zone';
 
